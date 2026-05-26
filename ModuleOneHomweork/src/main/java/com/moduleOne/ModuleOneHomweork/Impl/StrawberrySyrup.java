b4c574f0b1fb66d443e4695b4ea3dc1a9ec7b523
NaShruti <asshruti9389@gmail.com>
2026-05-26 08:02:37 +0530

    Spring Boot Changes for All Dependency Injection

--- ModuleOneHomweork/src/main/java/com/moduleOne/ModuleOneHomweork/Impl/StrawberrySyrup.java
+++ ModuleOneHomweork/src/main/java/com/moduleOne/ModuleOneHomweork/Impl/StrawberrySyrup.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("StrawberrySyrup")
+//@Qualifier("StrawberrySyrup")
 public class StrawberrySyrup implements Syrup {
 
     @Override
--- ModuleOneHomweork/src/main/java/com/moduleOne/ModuleOneHomweork/ModuleOneHomweorkApplication.java
+++ ModuleOneHomweork/src/main/java/com/moduleOne/ModuleOneHomweork/ModuleOneHomweorkApplication.java
@@ -1,18 +1,26 @@
 package com.moduleOne.ModuleOneHomweork;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class ModuleOneHomweorkApplication implements CommandLineRunner {
 
-	final CakeBaker cakeBaker;
+	/*final CakeBaker cakeBaker;
 
 	public ModuleOneHomweorkApplication(CakeBaker cakeBaker){
 		this.cakeBaker=cakeBaker;
-	}
+	}*/
+	@Autowired
+	Map<String,Frosting> frostingMap=new HashMap<>();
 
+	@Autowired
+	Map<String,Syrup> SyrupMap=new HashMap<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleOneHomweorkApplication.class, args);
@@ -20,6 +28,19 @@ public class ModuleOneHomweorkApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		cakeBaker.bakeCake();
+
+		for (var frostingMapService : frostingMap.entrySet()){
+
+			System.out.println(frostingMapService.getKey());
+			frostingMapService.getValue().getFrostingType();
+		}
+
+
+		for (var SyrupMapService : SyrupMap.entrySet()){
+
+			System.out.println(SyrupMapService.getKey());
+			SyrupMapService.getValue().getSyrupType();
+		}
+		//cakeBaker.bakeCake();
 	}
 }
